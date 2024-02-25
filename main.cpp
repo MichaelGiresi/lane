773f783d63f5ffad9b5d3a3a7e3a3e64bb75c447
MichaelGiresi <84191824+MichaelGiresi@users.noreply.github.com>
2024-02-24 19:47:33 -0500

    CRUD 2/24/24
    Create, Read, Update finished

--- main.cpp
+++ main.cpp
@@ -1,14 +1,48 @@
 #include <iostream>
 #include <unordered_map>
 #include <string>
+#include <fstream>
+#include <sstream>
+#include <limits>
 
 class DHTNode {
 private:
     std:: unordered_map<std::string, std::string> table;
+    std:: string filename = "dht_data.txt";
+
+    void saveToFile() {
+        std::ofstream outFile(filename);
+        for (const auto& pair : table) {
+            outFile << pair.first << "," << pair.second << std::endl;
+        }
+        outFile.close();
+    }
+
+    void loadFromFile() {
+        std::ifstream inFile(filename);
+        std::string line;
+        while (std::getline(inFile,line)) {
+            std::istringstream iss(line);
+            std::string key, value;
+            if (std::getline(iss, key, ',') && std::getline(iss, value)) {
+                table[key] = value;
+            }
+        }
+        inFile.close();
+    }
 
 public:
+
+    DHTNode() {
+        loadFromFile();
+    }
+
+    ~DHTNode() {
+        saveToFile();
+    }
     void store(const std::string& key, const std::string& value) {
         table[key] = value;
+        saveToFile();
         std::cout << "Stored key-value pair: " << key << " - " << value << std::endl;
     }
 
@@ -18,6 +52,17 @@ public:
             return it->second;
         } else {
             return "Key not found";
+        }
+    }
+
+    void displayAll() {
+        if (table.empty()) {
+            std::cout << "The table is empty" <<std::endl;
+        } else {
+            std::cout << "All stored key-value pairs:" << std::endl;
+            for (const auto& pair : table) {
+                std::cout << pair.first << " - " << pair.second << std::endl;
+            }
         }
     }
 };
@@ -31,8 +76,9 @@ int main() {
         std::cout << "\nDHT Node Menu:\n";
         std::cout << "1. Store key-value pair\n";
         std::cout << "2. Retrieve vaalue by key\n";
-        std::cout << "3. Exit\n";
-        std::cout << "Enter your choice (1-3): ";
+        std::cout << "3. Display all key-value pairs\n";
+        std::cout << "4. Exit\n";
+        std::cout << "Enter your choice (1-4): ";
         std::cin >> choice;
 
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
@@ -52,12 +98,15 @@ int main() {
                 std::cout << "Retrieved value: " << value << std::endl;
                 break;
             case '3':
+                node.displayAll();
+                break;
+            case '4':
                 std::cout << "Exiting program.\n";
                 break;
             default:
-                std::cout << "Invalid choice. Please enter 1, 2 or 3.\n";
+                std::cout << "Invalid choice. Please enter 1, 2, 3 or 4.\n";
         }
-    } while (choice != '3');
+    } while (choice != '4');
 
     return 0;
 }
